9471bfdbcf74e784a19f7c47ad600548ccd3beb3
Daniel <dmarlinghaus@gmail.com>
2023-03-17 20:23:43 +0100

    workaround for output file + some fixes (still bad request)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vidr
 Vulnerability Incompatible Dependencies Resolver
+
+## complications
+- Das Gradle Testproject und die Lib zerstören sich dauernd den build cache
+- GradleAPI Dependencies wie jackson oder http-client können nicht im Testprojekt referenziert werden
+- Lokale Entwicklung dadurch sehr aufwendig
--- src/main/java/de/daniel/marlinghaus/vidr/VidrPlugin.java
+++ src/main/java/de/daniel/marlinghaus/vidr/VidrPlugin.java
@@ -6,6 +6,7 @@ import static de.daniel.marlinghaus.vidr.VidrTasks.CREATE_VULNERABILITY_REPORT;
 import de.daniel.marlinghaus.vidr.task.CreateVulnerabilityReport;
 import de.daniel.marlinghaus.vidr.task.vo.ScanFormat;
 import de.daniel.marlinghaus.vidr.task.vo.ScanJob;
+import java.nio.file.Path;
 import org.cyclonedx.gradle.CycloneDxTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
@@ -21,6 +22,7 @@ public class VidrPlugin implements Plugin<Project> {
   public void apply(Project targetProject) {
     var tasks = targetProject.getTasks();
     String projectName = targetProject.getName();
+    Path reportPath = targetProject.getBuildDir().toPath().resolve("reports");
 
     // configure sbom creation
     CycloneDxTask createSbomTask = tasks.register(CREATE_SBOM.getName(),
@@ -29,7 +31,7 @@ public class VidrPlugin implements Plugin<Project> {
           sbomTask.setGroup(VidrGroups.REPORTING.getName());
           sbomTask.setProjectType("application");
           sbomTask.setSchemaVersion("1.4");
-          sbomTask.setDestination(targetProject.getBuildDir().toPath().resolve("reports").toFile());
+          sbomTask.setDestination(reportPath.toFile());
           sbomTask.setOutputName(
               String.format("%s-%s-sbom", projectName, targetProject.getVersion()));
           sbomTask.setOutputFormat("json");
@@ -38,23 +40,25 @@ public class VidrPlugin implements Plugin<Project> {
         }).get(); //TODO use output from previous cyclone Task in Project
 
     // configure vuln report creation
-    CreateVulnerabilityReport createVulnerabilityReportTask = tasks.register(CREATE_VULNERABILITY_REPORT.getName(), CreateVulnerabilityReport.class,
+    CreateVulnerabilityReport createVulnerabilityReportTask = tasks.register(
+        CREATE_VULNERABILITY_REPORT.getName(), CreateVulnerabilityReport.class,
         (vulnReportTask) -> {
+          ScanJob scanJob = ScanJob.builder()
+              .applicationName(projectName)
+              .format(ScanFormat.SBOM)
+              .stage(
+                  "local") //TODO get from environment variables (SPRING_PROFILES_ACTIVE) or default local
+              .pipelineRun("") //TODO get from environment variables or default empty
+              .build();
+
           vulnReportTask.setGroup(VidrGroups.REPORTING.getName());
           // define scan service default url
 //          vulnReportTask.getServiceUrl().set(
 //              vulnReportTask.getServiceUrl().getOrElse(URI.create("http://localhost:8100")));
           vulnReportTask.setScanObjectFile(
               createSbomTask.getDestination().get().toPath().resolve(createSbomTask.getOutputName()
-                  .get()));
-          vulnReportTask.setScanJob(
-              ScanJob.builder()
-                  .applicationName(projectName)
-                  .format(ScanFormat.SBOM)
-                  .stage("local") //TODO get from environment variables (SPRING_PROFILES_ACTIVE) or default local
-                  .pipelineRun("") //TODO get from environment variables or default empty
-                  .build()//TODO use enum
-          );
+                  .get() + ".json"));
+          vulnReportTask.setScanJob(scanJob);
 
           // define execution order
           vulnReportTask.dependsOn(createSbomTask);
